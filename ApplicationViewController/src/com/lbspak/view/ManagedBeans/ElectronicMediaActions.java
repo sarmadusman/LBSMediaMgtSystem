2acf21fd5c8cbc25b8a20ee5bc494c684d44070a
Usman <u-s-m-a-n@outlook.com>
2016-10-28 06:45:11 -0700

    Added Dynamic colomns functinoality

--- ApplicationViewController/src/com/lbspak/view/ManagedBeans/ElectronicMediaActions.java
+++ ApplicationViewController/src/com/lbspak/view/ManagedBeans/ElectronicMediaActions.java
@@ -15,12 +15,19 @@ import org.joda.time.DurationFieldType;
 
 public class ElectronicMediaActions {
     private HashMap columns;
+    private HashMap columnLables;
     private RichInputDate dateFrom;
     private RichInputDate dateTo;
 
     public ElectronicMediaActions() {
         if (columns == null) {
             columns = new HashMap();
+        }
+        if (columnLables == null) {
+            columnLables = new HashMap();
+        }
+        for (int i=1; i<=60; i++) {
+            columns.put("Day "+i, false);
         }
     }
 
@@ -39,6 +46,8 @@ public class ElectronicMediaActions {
             cal.add(Calendar.DATE, 1);
             dayNo = ((dayNo==0) ? cal.get(Calendar.DAY_OF_MONTH): (++dayNo));
             this.columns.put("Day "+dayNo, true);
+            System.out.println(cal.getTime().toString());
+            this.columnLables.put("Day "+dayNo, cal.getTime().toString());
         }
         
         
@@ -53,4 +62,12 @@ public class ElectronicMediaActions {
     public void setDateTo(RichInputDate dateTo) {this.dateTo = dateTo;}
 
     public RichInputDate getDateTo() {return dateTo;}
+
+    public void setColumnLables(HashMap columnLables) {
+        this.columnLables = columnLables;
+    }
+
+    public HashMap getColumnLables() {
+        return columnLables;
+    }
 }
